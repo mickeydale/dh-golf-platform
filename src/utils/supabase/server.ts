5e07dea6cc7ae4d4364d9f357472344189de9fe9
Anurag Dale <anuragdale43@gmail.com>
2026-03-26 20:24:02 +0530

    Make createClient function asynchronous

--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -1,19 +1,19 @@
 import { createServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 
-export function createClient() {
+export async function createClient() {
+  const cookieStore = await cookies()
+
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        async get(name: string) {
-          const cookieStore = await cookies()
+        get(name: string) {
           return cookieStore.get(name)?.value
         },
-        async set(name: string, value: string, options: any) {
+        set(name: string, value: string, options: any) {
           try {
-            const cookieStore = await cookies()
             cookieStore.set({ name, value, ...options })
           } catch (error) {
             // The `set` method was called from a Server Component.
@@ -21,9 +21,8 @@ export function createClient() {
             // user sessions.
           }
         },
-        async remove(name: string, options: any) {
+        remove(name: string, options: any) {
           try {
-            const cookieStore = await cookies()
             cookieStore.set({ name, value: '', ...options })
           } catch (error) {
             // The `remove` method was called from a Server Component.
